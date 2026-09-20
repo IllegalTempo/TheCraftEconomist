0940017908d960d839b0de255c45a2aa9d777220
IllegalTempo <jedtsui0704@gmail.com>
2026-09-20 22:00:24 +0100

    feat: label blueprint lifecycle states

--- src/main/java/com/jedts/theeconomist/blueprint/BlueprintStackData.java
+++ src/main/java/com/jedts/theeconomist/blueprint/BlueprintStackData.java
@@ -6,6 +6,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
+import net.minecraft.network.chat.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +29,14 @@ public record BlueprintStackData(BlueprintState state, BlueprintDesign design, B
     }
 
     public static void setDesigned(ItemStack stack, BlueprintDesign design) {
+        stack.set(DataComponents.CUSTOM_NAME, Component.literal(BlueprintDisplayName.forState(BlueprintState.DESIGNED)));
         write(stack, new BlueprintStackData(BlueprintState.DESIGNED, design, null));
     }
 
     public static void setPlanned(ItemStack stack, BlueprintPlacement placement) {
         BlueprintStackData current = read(stack);
         if (current.design == null) throw new IllegalStateException("cannot plan an empty blueprint");
+        stack.set(DataComponents.CUSTOM_NAME, Component.literal(BlueprintDisplayName.forState(BlueprintState.PLANNED)));
         write(stack, new BlueprintStackData(BlueprintState.PLANNED, current.design, placement));
     }
 
